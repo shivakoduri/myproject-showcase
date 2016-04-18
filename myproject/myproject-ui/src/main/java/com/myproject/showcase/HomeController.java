337046bdc1c908029481b4d14b91fd02579091f9
shiva <koduri.shiva@gmail.com>
2016-04-18 21:12:31 +0530

    adding more on spring security

--- myproject/myproject-ui/src/main/java/com/myproject/showcase/HomeController.java
+++ myproject/myproject-ui/src/main/java/com/myproject/showcase/HomeController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -23,53 +22,16 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = {"/","/welcome**"}, method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView welcome() {
 		logger.info("--Inside the landing page...");
 		
 		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("title", "Spring Project Demo");
 		model.addObject("message","This is welcome page");
 		model.setViewName("home");
 		
 		return model;
 	}
-	
-	@RequestMapping(value="/admin**", method = RequestMethod.GET)
-	public ModelAndView adminPage(){
-		
-        logger.info("--Inside the admin...");
-		
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
-		model.addObject("message","This is protected page");
-		model.setViewName("admin");
-		
-		return model;
-	}
-	
-	
-	@RequestMapping(value="/login",method=RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value="error",required=false) String error,
-			@RequestParam(value="logout",required=false) String logout){
-		
-		logger.info("--Inside login page request...");
-		
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username and password!");
-		}
-
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
-
-		return model;
-
-		
-	}
-	
-
 
 }
